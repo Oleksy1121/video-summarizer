c7ff253cce37c9ee2b29e7e24da48b5455261747
Oleksy1121 <oleszczyk.dev@gmail.com>
2025-10-06 17:05:12 +0200

    feat: implement video summarization service with audio download and validation

--- video_summarizer/constants.py
+++ video_summarizer/constants.py
@@ -3,6 +3,8 @@ SUMMARIZE_MODEL = "gpt-4o-mini"
 
 VIDEO_URL = "https://www.youtube.com/watch?v=TMkoX1kfyDs&t"
 AUDIO_FILENAME = 'data/audio/audio_from_wideo.mp3'
+MAX_DURATION = 1800 # 30 minutes
+MAX_SIZE = 30 * 1024 * 1024 # 30MB
 
 SYSTEM_MESSAGE = "You are an assistant that summarizes YouTube videos by highlighting the most important points and events, in clear Markdown format."
 USER_PROMPT = f"Below is the transcript of a YouTube video. Please write a summary describing the key moments, main ideas, and highlights in a structured way (using bullet points or short sections)."
--- video_summarizer/download.py
+++ video_summarizer/download.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import logging
 from video_summarizer.constants import AUDIO_FILENAME
 
 
@@ -14,14 +15,19 @@ def download_audio(url: str, audio_filename: str = AUDIO_FILENAME) -> None:
         None
     """
     if os.path.exists(audio_filename):
+        logging.info(f"[download_audio] Downloading audio from: {url} -> {audio_filename}")
         os.remove(audio_filename)
         # print("Old audio file has been deleted.")
         
-    subprocess.run([
+    result = subprocess.run([
         "yt-dlp",
         "-x",                        # only audio
         "--audio-format", "mp3",     # convert to mp3
         "--force-overwrites",        # forve override file
         "-o", audio_filename,        # save file
         url
-    ])
+    ], capture_output=True, text=True)
+
+    logging.info(f"[download_audio] returncode={result.returncode}")
+    logging.info(f"[download_audio] stdout={result.stdout[:300]}")
+    logging.info(f"[download_audio] stderr={result.stderr}")
